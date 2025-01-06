23ca7fad2725c626b31f315dd2af0fbfa9519b6b
Sniffedpcs <omichaels@usa.com>
2025-01-06 14:48:33 +0000

    Day 1

---
+++ BasicArithmetics.py
@@ -0,0 +1,26 @@
+a = 10
+b = 20
+results = a * b
+
+print(results)
+
+
+a = 500
+b = 20
+results = a / b
+
+print(results)
+
+
+a = 500
+b = 20
+results = a + b
+
+print(results)
+
+
+a = 500
+b = 20
+results = a - b
+
+print(results)
---
+++ HelloWorld.py
@@ -0,0 +1,1 @@
+print('Hello World, I am Sniffedpcs, A.K.A Pun Hunk')
