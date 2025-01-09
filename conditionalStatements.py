da4ba44c6d38be8e9f4260c488cea798f4693eab
Sniffedpcs <omichaels@usa.com>
2025-01-09 00:38:10 +0000

    Day 3

---
+++ conditionalStatements.py
@@ -0,0 +1,8 @@
+userAge = int(input("Enter Your Age "))
+
+if userAge < 18:
+    print("Sorry, You Are Too Young To Access This Page!")
+elif userAge > 18:
+    print("Dear Favorite Customer, Welcome To BangBros!")
+else:
+    print("Enter Your Age ")
